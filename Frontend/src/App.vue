8eddbea556fb1b26b996503bf0965c5bf9853880
Isak410 <Isakstenep@gmail.com>
2024-06-03 13:58:36 +0200

    fiks

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,26 +1,51 @@
 <script>
-
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-  export default {
-    setup() {
-        const inputData = ref('')
+export default {
+  setup() {
+    const inputData = ref('');
+    const storagedata = ref([]);
 
-        const insertdata = async(userdata) => {
-          const result = await axios.post(`http://localhost:8080/insertdata`,{userdata:userdata})
-          console.log(result)
+    const insertdata = async (userdata) => {
+      try {
+        const result = await axios.post('http://localhost:8080/insertdata', { userdata: userdata });
+        console.log(result);
+        if (result.data.success) {
+          const newdata = await getData();
+          storagedata.value = newdata;
         }
+      } catch (error) {
+        console.error('Error inserting data:', error);
+      }
+    };
 
-        const submitData = () => {
-          insertdata(inputData.value)
-        }
+    const submitData = () => {
+      insertdata(inputData.value);
+      inputData.value = '';  // Clear input after submission
+    };
 
-        return {
-          submitData,
-          inputData
-        };
-    },
+    const getData = async () => {
+      try {
+        const result = await axios.get('http://localhost:8080/getdata');
+        console.log(result);
+        return result.data.storagedata;
+      } catch (error) {
+        console.error('Error getting data:', error);
+        return [];
+      }
+    };
+
+    onMounted(async () => {
+      storagedata.value = await getData();
+    });
+
+    return {
+      submitData,
+      inputData,
+      storagedata
+    };
+  },
 };
 </script>
 
@@ -28,4 +53,7 @@ import axios from 'axios';
   <h1>Vue adøaldkøla</h1>
   <input v-model="inputData">
   <button @click="submitData">Submit</button>
+  <ul>
+    <li v-for="(element, index) in storagedata" :key="index">{{ element }}</li>
+  </ul>
 </template>
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,39 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const fs = require('fs')
-const port = 8080
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const port = 8080;
 
-const storagefile = './storage.json'
+const storagefile = './storage.json';
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/testroute', (req,res) => {
-    res.json('testrouteresult')
-})
+app.get('/testroute', (req, res) => {
+    res.json('testrouteresult');
+});
 
-app.post('/insertdata', async(req,res) => {
-    const {userdata} = req.body
-    console.log('userdata: '+userdata)
-    fs.readFile(storagefile,"utf8",function(err,unparseddata) {
-        if(err){res.json({success:false});return}
-        const data = JSON.parse(unparseddata)
-        data.storagedata.push(userdata)
-        fs.writeFile(storagefile,JSON.stringify(data),function(err,data) {
-            if(err)res.json({success:false})
-            else res.json({success:true})
-        })  
-    })
-})
+app.post('/insertdata', async (req, res) => {
+    const { userdata } = req.body;
+    console.log('userdata: ' + userdata);
+    fs.readFile(storagefile, "utf8", function (err, unparseddata) {
+        if (err) { res.json({ success: false }); return; }
+        const data = JSON.parse(unparseddata);
+        data.storagedata.push(userdata);
+        fs.writeFile(storagefile, JSON.stringify(data), function (err) {
+            if (err) res.json({ success: false });
+            else res.json({ success: true });
+        });
+    });
+});
 
-async function testfunc() {
-    fs.readFile(storagefile,"utf8",function(err,data) {
-        console.log(data)
-    })
-}
-
-testfunc()
+app.get('/getdata', async (req, res) => {
+    fs.readFile(storagefile, 'utf8', function (err, data) {
+        if (err) res.json({ success: false, data: null });
+        else res.json({ success: true, storagedata: JSON.parse(data).storagedata });
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
